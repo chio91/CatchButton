96b1fe7b3e39c044c7524fd173d1912165f2c312
지민 최 <jimin_0128@daum.net>
2026-03-12 17:27:55 +0900

    과제 3단계 (점수 기능 추가/성공할 떄마다 크기 감소)

--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,8 @@ namespace CatchButton
 {
     public partial class Form1 : Form
     {
+        int score = 250;
+
         public Form1()
         {
             InitializeComponent();
@@ -18,12 +20,17 @@ namespace CatchButton
             int next_Y = rd.Next(0, Y_max - chase_but.Height);
 
             chase_but.Location = new Point(next_X, next_Y);
-            this.Text = $"버튼 위치 : ({next_X},{next_Y})";
+            score -= 10;
+            this.Text = $"버튼 위치 : ({next_X},{next_Y}) / 점수 : {score}";
         }
 
         private void chase_but_Click(object sender, EventArgs e)
         {
             MessageBox.Show("축하합니다~!");
+            score += 100;
+            chase_but.Width = (int)(chase_but.Width * 0.9);
+            chase_but.Height = (int)(chase_but.Height * 0.9);
+            this.Text = $"버튼 위치 : ({chase_but.Location.X},{chase_but.Location.Y}) / 점수 : {score}";
         }
     }
 }
