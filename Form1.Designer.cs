c75b138c6c119dba70f2904df9dd938f05afc883
지민 최 <jimin_0128@daum.net>
2026-03-12 16:29:14 +0900

    과제 1단

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            chase_but = new Button();
             SuspendLayout();
+            // 
+            // chase_but
+            // 
+            chase_but.BackColor = Color.LawnGreen;
+            chase_but.Font = new Font("맑은 고딕", 9.857143F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            chase_but.ForeColor = Color.Navy;
+            chase_but.Location = new Point(878, 402);
+            chase_but.Name = "chase_but";
+            chase_but.Size = new Size(250, 75);
+            chase_but.TabIndex = 0;
+            chase_but.Text = "나를 잡아봐";
+            chase_but.UseVisualStyleBackColor = false;
+            chase_but.Click += chase_but_Click;
+            chase_but.MouseEnter += chase_but_MouseEnter;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(12F, 30F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1252, 704);
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(1256, 611);
+            Controls.Add(chase_but);
             Name = "Form1";
-            Text = "Form1";
+            Text = "버튼 잡기 게임";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button chase_but;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -7,7 +7,21 @@ namespace CatchButton
             InitializeComponent();
         }
 
-        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void chase_but_MouseEnter(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+
+            int X_max = ClientSize.Width;
+            int Y_max = ClientSize.Height;
+
+            int next_X = rd.Next(0, X_max - chase_but.Width);
+            int next_Y = rd.Next(0, Y_max - chase_but.Height);
+
+            chase_but.Location = new Point(next_X, next_Y);
+            this.Text = $"버튼 위치 : ({next_X},{next_Y})";
+        }
+
+        private void chase_but_Click(object sender, EventArgs e)
         {
 
         }
